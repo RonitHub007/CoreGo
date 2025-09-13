386f4354cb530507cd3571ce2ef619b71d9973bc
RonitCodes7 <rroy8518@gmail.com>
2025-09-14 00:27:18 +0530

    practice go routine and select

--- main.go
+++ main.go
@@ -1,82 +1,66 @@
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func produceNumbers(mainCh chan int) {
+// 	for i := 0; i < 20; i++ {
+// 		mainCh <- i
+// 	}
+// 	close(mainCh)
+// }
+
+// func squareNumbers(workerCh chan int, mainCh chan int) {
+// 	// length := len(mainCh)
+
+// 	for i := 0; i < 20; i++ {
+// 		data := <-mainCh
+// 		workerCh <- data * data
+// 	}
+// 	close(workerCh)
+// }
+// func main() {
+// 	ch := make(chan int)
+// 	ch2 := make(chan int)
+// 	go produceNumbers(ch)
+
+// 	go squareNumbers(ch2, ch)
+
+// 	// time.Sleep(20 * time.Second)
+// 	for i := 0; i < 20; i++ {
+// 		fmt.Println(<-ch2)
+// 	}
+// }
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func speak(arg string, ch chan string) {
-	ch <- arg
-}
 func main() {
-	// 1. Concurrency Pattern 1 Race Condition
 
-	// var data int
-	// go func() {
-	// 	data++
-	// }()
-	// time.Sleep(1 * time.Second)
-	// if data == 0 {
-	// 	fmt.Printf("the value is %v.\n", data)
-	// }
+	one := make(chan string)
+	two := make(chan string)
 
-	//----------------------------------------
+	go func() {
+		time.Sleep(2 * time.Second)
+		two <- "Two"
+	}()
 
-	//2. Memory access Synchronization
-	// var memeoryAccess sync.Mutex
-	// var data int
-	// go func() {
-	// 	memeoryAccess.Lock()
-	// 	data += 1
-	// 	memeoryAccess.Unlock()
-	// }()
-	// memeoryAccess.Lock()
-	// if data == 0 {
-	// 	fmt.Printf("the value is %v.\n", data)
-	// } else {
-	// 	fmt.Printf("the value is %v.\n", data)
-	// }
-	// memeoryAccess.Unlock()
-	//-----------------------------------------
+	go func() {
+		time.Sleep(1 * time.Second)
+		one <- "One"
+	}()
 
-	// 3. Channel
-
-	// var ch chan T
-	// chan means channel and T is the tyope of data we are going to send
-	// var ch chan string
-	// ch2 := make(chan string)
-	// fmt.Println(ch, ch2)
-
-	// Declaring and passing and receving data through channel(Unbuffered)
-	// ch := make(chan string)
-	// go speak("Hello World", ch)
-
-	// data := <-ch
-	// fmt.Println(data)
-
-	// Buffered Channel
-
-	// ch := make(chan int, 3)
-
-	// fmt.Println("Buffer initiation")
-	// ch <- 1
-	// ch <- 2
-	// ch <- 3
-	// fmt.Println("Buffer receievs starts")
-
-	// fmt.Println(<-ch)
-	// fmt.Println(<-ch)
-	// fmt.Println(<-ch)
-
-	// Directional Channel
-	ch := make(chan bool)
-
-	go sayTrue(true, ch)
-
-	// ch2 := make(chan bool)
-
-	// ch2 <- <-ch
-	fmt.Println(<-ch)
-
-}
-
-func sayTrue(word bool, ch chan<- bool) {
-	ch <- word
+	select {
+	case result := <-one:
+		fmt.Println("Recived: ", result)
+	case result := <-two:
+		fmt.Println("Recived: ", result)
+	}
+	close(one)
+	close(two)
 }
